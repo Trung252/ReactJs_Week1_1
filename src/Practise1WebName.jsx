41d646203ccce6a9189d194296958a821c72112a
Trung252 <pkt02052000@gmail.com>
2023-02-20 18:12:31 +0700

    Tạo Element thể hiện thông tin của Trình duyệt bạn đang sử dụng

---
+++ src/Practise1WebName.jsx
@@ -0,0 +1,11 @@
+import web from "./data";
+
+
+const Practise1WebName = () => {
+    return (
+        <table>
+            <h4>Browser's details: {navigator.userAgent}</h4>
+        </table>
+    )
+}
+export default Practise1WebName
---
+++ src/data.js
@@ -0,0 +1,9 @@
+const web = [
+    {
+       app1: 'Google/5.0 (Macintosh; Intel Mac OS X 10_15_6)',
+       app2: 'AppleWebKit/537.36(KHTML, like Gecho)',
+       app3: 'Chrome/93.0.4577.63 Safari/537.36'
+
+    }
+]
+export default web
